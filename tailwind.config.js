59e929c5b1795c44396cfdd47c00e5cd61104da2
pfisterNZ <pfistercontacto@gmail.com>
2025-05-17 15:16:32 -0300

    Components and Projects added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{astro,html,js,jsx,ts,tsx,vue}",
+    ],
+    theme: {
+      extend: {
+        animation: {
+          'spin-slow': 'spin 4s linear infinite',
+        },
+      },
+    },
+  plugins: [],
+}
+
+
