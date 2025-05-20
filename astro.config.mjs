23343467e0a1f5b20f311c183dd2a89740e402ad
pfisterNZ <pfistercontacto@gmail.com>
2025-05-20 19:06:39 -0300

    hola

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 
+
 // https://astro.build/config
 export default defineConfig({
-
-  outDir: "dist",
-
-  site: "https://pfisterNZ.github.io",
-  base: "/astro-test",
 
   vite: {
     plugins: [tailwindcss()],
   },
   devToolbar: {
     enabled: false,
-  }
+  },
 });
